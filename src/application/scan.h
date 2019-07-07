5385e252be9166996dc0a98a99e7e1287ae0a812
CWTeam <cwteam@bk.ru>
2019-07-07 11:54:01 +0700

    Clear project

--- src/application/scan.h
+++
@@ -1,9 +0,0 @@
-#ifndef SCAN_H_
-#define SCAN_H_
-
-
-inline uint8_t sensorTouch(void) { return !(SENSOR_PORT->IDR & SENSOR_PIN); }
-void initSensor(void);
-void scanMode(void);
-
-#endif /* SCAN_H_ */
